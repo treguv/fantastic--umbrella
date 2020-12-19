460aff463eaf4abfbc8bac61359a3f1bd853d6ed
treguv <vladislavtregubov00@gmail.com>
2020-12-19 14:22:29 -0800

    Built models for all objects

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,44 @@ class Product extends Model {}
 Product.init(
   {
     // id
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    //Product name
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    //price
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        // check to make sure the value is a decimal
+        isDecimal: true,
+      },
+    },
+    //stock
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    //categoty id
+    category_id: {
+      type: DataTypes.INTEGER, //give data type of col
+      references: {
+        //set the references
+        model: "category", // the model in question
+        key: "id", // the field from the model
+      },
+    },
   },
   {
     sequelize,
